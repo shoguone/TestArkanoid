38e907cfc9b78191b85a193f96704217cbf1e76f
Vladimir <Alemasov>
2016-10-21 18:09:52 +0700

    Network tutorial init

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMove>();
+        if (hitPlayer != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        
+        var x = Input.GetAxis("Horizontal")*0.1f;
+        var z = Input.GetAxis("Vertical")*0.1f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+//            Fire();
+            CmdFire();
+
+        transform.Translate(x, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+
+        NetworkServer.Spawn(bullet);
+
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 2.0f);        
+    }
+
+    void Fire()
+    {
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 2.0f);        
+    }
+}
