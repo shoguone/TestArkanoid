3845ddcdfbcc04bcdfaa702ac694f648c15e3a3b
Vladimir <Alemasov>
2016-10-24 13:23:17 +0700

    Network tutorial final

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
     void OnCollisionEnter(Collision collision)
     {
         var hit = collision.gameObject;
-        var hitPlayer = hit.GetComponent<PlayerMove>();
+//        var hitPlayer = hit.GetComponent<PlayerMove>();
+        var hitPlayer = hit.GetComponent<Combat>();
         if (hitPlayer != null)
         {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+
             Destroy(gameObject);
         }
     }
